ab38a61cfb22a4e10a347d8a46f550bbe3b1c952
0xMazout <lucas.fabre66@gmail.com>
2023-10-25 02:06:54 +0200

    Growth coverage

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -8,9 +8,14 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant SEND_VALUE = 0.1 ether;
+
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public {
@@ -27,5 +32,17 @@ contract FundMeTest is Test {
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert(); //next line should revert !
+        fundMe.fund(); // send 0 value !
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFundedd(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
